928ec100ecb9b894ffbd1e87dce88d49cde03dc8
LeeJongHyeons <52764901+LeeJongHyeons@users.noreply.github.com>
2019-08-14 17:52:27 +0900

    Add files via upload

---
+++ content theorem.py
@@ -0,0 +1,24 @@
+'''
+[KERAS]
+
+y = wx+b
+x,y값을 이용해 w,b를 구함
+
+DNN: Deep
+
+RNN: 시계열 => 순차적 추출        LSTM => Conv1D
+
+CNN: 이미지 분석 => 특성 추출
+
+ML: model.fit(x_train, x_test) 가능!
+
+지도: SVM, DT, RF, GB, XGB 등등..다양한 모델 컬럼에 대한 중요도 분석
+비지도: y값 없이 훈련 PCA(컬럼 축소), Autoencoder(encoder, decoder)
+
+[ML]         [KERAS]
+compile      compile
+fit          fit
+score        evaluate
+predict      predict
+
+'''
